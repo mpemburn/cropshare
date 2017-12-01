4045c55299f5f89ae2c79efb7c43efd36cbdb18f
Mark Pemburn <mark@pemburn.com>
2017-11-30 22:20:51 -0500

    Create DOMObserver to detect imgedit-menu and add button.

--- js/cropshare.js
+++ js/cropshare.js
@@ -1,38 +1,46 @@
-jQuery(document).ready(function ($) {
-    function findClass(className, callback) {
-        // Notify me of everything!
-        var observerConfig = {
-            attributes: true,
-            childList: true,
-            characterData: true,
-            subtree: true
-        };
-
-        // Node, config
-        // In this case we'll listen to all changes to body and child nodes
-        var targetNode = document.body;
-
-        var observer = new MutationObserver(function(mutations) {
+var DOMObserver = {
+    done: false,
+    observer: null,
+    callback: null,
+    className: '',
+    config: {
+        attributes: true,
+        childList: true,
+        characterData: true,
+        subtree: true
+    },
+    targetNode: document.body,
+    init: function(options) {
+        $.extend(this, options);
+        this.observe()
+    },
+    observe: function() {
+        var self = this;
+        this.observer = new MutationObserver(function(mutations) {
             mutations.forEach(function(mutation) {
-                if (mutation.attributeName == 'class') {
-                    if (mutation.target.className.indexOf(className) !== -1) {
-                        callback();
-                        return;
+                if (mutation.attributeName == 'class' && !self.done) {
+                    if (mutation.target.className.indexOf(self.className) !== -1) {
+                        self.callback();
+                        self.done = true;
                     }
                 }
             });
         });
-
-
-        observer.observe(targetNode, observerConfig);
+        this.observer.observe(this.targetNode, this.config);
     }
-    var found = findClass('imgareaselect-outer', function() {
-        //alert('You got it!');
-    });
-        $('.imgedit-menu').ready(function () {
-            //alert('wtf');
-        if ($('.imgedit-menu').is('*')) {
-            $('.imgedit-menu').append('<button type="button" class=""><span class="screen-reader-text">Crop and download</span></button>');
+}
+
+jQuery('.imgedit-menu').ready(function ($) {
+    var domObserver = Object.create(DOMObserver);
+    domObserver.init({
+        done: false,
+        className: 'imgareaselect-outer',
+        callback: function() {
+            var foo = 'bar!';
+            $('.imgedit-menu').append('<button type="button" id="cropshare" class="fa a-share-square-o"><span class="screen-reader-text">Crop and download</span></button>');
+            $('#cropshare').off().on('click', function() {
+                alert('Yessssss!!!');
+            })
         }
     });
 });
