4f915ccb161b6402db9814cb3cb9db3f6d924a37
Mark Pemburn <mark@pemburn.com>
2017-12-05 07:44:30 -0500

    Refining cropping algorithm.

--- cropshare.php
+++ cropshare.php
@@ -16,19 +16,19 @@ class CropShare
 
     public function __construct()
     {
-        $this->add_actions();
+        $this->addActions();
     }
 
-    public function cropshare_image_editor_save_pre($image, $post_id)
+    public function cropshareImageEditorSavePre($image, $post_id)
     {
         $this->wp_image = $image;
         $this->post_id = $post_id;
-        $this->image_url = $this->get_image_url($image);
+        $this->image_url = $this->getImageUrl($image);
 
         return $image;
     }
 
-    public function enqueue_assets()
+    public function enqueueAssets()
     {
         wp_enqueue_style('fontawesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
         wp_enqueue_style('cropshare', plugin_dir_url(__FILE__) . 'css/cropshare.css');
@@ -36,18 +36,18 @@ class CropShare
         wp_enqueue_script('cropshare');
     }
 
-    public function handle_cropshare_ajax()
+    public function handleCropshareAjax()
     {
         $post_id = $_REQUEST['post_id'];
         $selection = json_decode(stripslashes($_REQUEST['selection']));
         $image_width = $_REQUEST['width'];
         $image_height = $_REQUEST['height'];
 
-        $image_path = $this->get_image_path_from_url($post_id);
+        $image_path = $this->getImagePathFromUrl($post_id);
         if (!is_null($image_path)) {
-            $image = $this->create_image($image_path);
+            $image = $this->createImage($image_path);
             if (!is_null($image)) {
-                $this->save_cropped_image($image, $selection, $image_width, $image_height);
+                $this->saveCroppedImage($image, $selection, $image_width, $image_height);
             }
         }
 
@@ -65,26 +65,26 @@ class CropShare
     /**
      * Add WordPress actions and filters
      */
-    protected function add_actions()
+    protected function addActions()
     {
         // Enqueue the js and css needed for this plugin
-        add_action('admin_enqueue_scripts', [$this, 'enqueue_assets']);
+        add_action('admin_enqueue_scripts', [$this, 'enqueueAssets']);
 
         // Set up AJAX handler
-        add_action('wp_ajax_handle_cropshare_ajax', [$this, 'handle_cropshare_ajax']);
+        add_action('wp_ajax_handle_cropshare_ajax', [$this, 'handleCropshareAjax']);
 
         // Grab the image and post info
-        add_filter('image_editor_save_pre', [$this, 'cropshare_image_editor_save_pre'], 10, 2);
+        add_filter('image_editor_save_pre', [$this, 'cropshareImageEditorSavePre'], 10, 2);
     }
 
     /**
      * @param $image_url
      * @return null|resource
      */
-    protected function create_image($image_url)
+    protected function createImage($image_url)
     {
         $ext = pathinfo($image_url, PATHINFO_EXTENSION);
-        $image_path = $this->get_image_from_url($image_url);
+        $image_path = $this->getImageFromUrl($image_url);
 
         if (!file_exists($image_path)) {
             return null;
@@ -111,7 +111,7 @@ class CropShare
      * @param $post_id
      * @return string|null
      */
-    protected function get_image_path_from_url($post_id)
+    protected function getImagePathFromUrl($post_id)
     {
         $image_info = wp_get_attachment_image_src($post_id, '');
 
@@ -122,7 +122,7 @@ class CropShare
      * @param $image
      * @return string|null
      */
-    protected function get_image_url($image)
+    protected function getImageUrl($image)
     {
         $image_url = null;
         $unique_id = uniqid();
@@ -140,8 +140,9 @@ class CropShare
         return $image_url;
     }
 
-    protected function get_image_from_url($image_url)
+    protected function getImageFromUrl($image_url)
     {
+        $file_path = null;
         $parsed = parse_url($image_url);
 
         $uploads = wp_upload_dir();
@@ -156,7 +157,7 @@ class CropShare
         return $file_path;
     }
 
-    protected function save_cropped_image($origin_image, $selection, $origin_width, $origin_height)
+    protected function saveCroppedImage($origin_image, $selection, $origin_width, $origin_height)
     {
         $uploads = wp_upload_dir();
         if (is_array($uploads)) {
@@ -166,8 +167,10 @@ class CropShare
         $dest_y = $selection->y;
         $dest_width = $selection->w;
         $dest_height = $selection->h;
-        $dest_image = imagecreatetruecolor($origin_width, $origin_height);
-        if (imagecopyresampled($dest_image, $origin_image, 0, 0, $dest_x, $dest_y, $origin_width, $origin_height, $origin_width, $origin_height)) {
+        $scale = 1 / $this->imageGetPreviewRatio( imagesx( $origin_image ), imagesy( $origin_image ) ); // discard preview scaling
+        $dest_image = $this->cropImageResource( $origin_image, $selection->x * $scale, $selection->y * $scale, $selection->w * $scale, $selection->h * $scale );
+
+        if (imagecopyresampled($dest_image, $origin_image, 0, 0, $dest_x, $dest_y, $dest_width, $dest_height, $origin_width, $origin_height)) {
             if (imagepng($dest_image, $dest_path)) {
                 imagedestroy($dest_image);
             }
@@ -175,7 +178,32 @@ class CropShare
 
     }
 
-    protected function imagecreatefrombmp($p_sFile)
+    protected function cropImageResource($img, $x, $y, $w, $h) {
+        $dst = $this->createTrueColorImage($w, $h);
+        if ( is_resource($dst) ) {
+            if ( imagecopy($dst, $img, 0, 0, $x, $y, $w, $h) ) {
+                imagedestroy($img);
+                $img = $dst;
+            }
+        }
+        return $img;
+    }
+
+    protected function createTrueColorImage($width, $height) {
+        $img = imagecreatetruecolor($width, $height);
+        if ( is_resource($img) && function_exists('imagealphablending') && function_exists('imagesavealpha') ) {
+            imagealphablending($img, false);
+            imagesavealpha($img, true);
+        }
+        return $img;
+    }
+
+    function imageGetPreviewRatio($w, $h) {
+        $max = max($w, $h);
+        return $max > 400 ? (400 / $max) : 1;
+    }
+
+    protected function imageCreateFromBMP($p_sFile)
     {
         //    Load the image into a string
         $file = fopen($p_sFile, "rb");
